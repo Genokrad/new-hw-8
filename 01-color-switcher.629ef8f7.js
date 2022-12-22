ef4051742bc89e214ed0c45184d5c9230507b535
Genokrad <104651129+Genokrad@users.noreply.github.com>
2022-12-22 18:40:55 +0000

    Deploying to gh-pages from @ Genokrad/new-hw-8@f8d72ed7b46d13ff8f34945ac3c9979501518621 🚀

---
+++ 01-color-switcher.629ef8f7.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]"),d=document.querySelector("body");function r(n){timerId=setInterval((()=>{d.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),500),e.disabled=!0,t.disabled=!1,e.removeEventListener("click",r)}e.addEventListener("click",r),t.addEventListener("click",(function(d){clearInterval(timerId),e.disabled=!1,t.disabled=!0,e.addEventListener("click",r)})),t.disabled=!0;
+//# sourceMappingURL=01-color-switcher.629ef8f7.js.map
