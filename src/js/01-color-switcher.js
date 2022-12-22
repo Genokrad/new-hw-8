f8d72ed7b46d13ff8f34945ac3c9979501518621
Genokrad <zeeper90@gmail.com>
2022-12-22 20:40:32 +0200

    new commit

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
+const bodyBcg = document.querySelector('body');
 
+btnStart.addEventListener('click', onBtnStartClick);
+btnStop.addEventListener('click', onBtnSstopClick);
+
+btnStop.disabled = true;
+
+function onBtnStartClick(event) {
+  timerId = setInterval(() => {
+    bodyBcg.style.backgroundColor = getRandomHexColor();
+  }, 500);
+  btnStart.disabled = true;
+  btnStop.disabled = false;
+  btnStart.removeEventListener('click', onBtnStartClick);
+}
+
+function onBtnSstopClick(event) {
+  clearInterval(timerId);
+  btnStart.disabled = false;
+  btnStop.disabled = true;
+  btnStart.addEventListener('click', onBtnStartClick);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,48 @@
+const form = document.querySelector('.form');
+
+form.addEventListener('input', createInputObj);
+
+const inputKey = {};
+
+function createInputObj(event) {
+  const { name, value } = event.target;
+  if (name) {
+    inputKey[name] = value;
+  }
+  form.addEventListener('submit', formHandler);
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      } else {
+        reject(`❌ Rejected promise ${position} in ${delay}ms`);
+      }
+    }, delay);
+  });
+  promise
+    .then(value => {
+      console.log(value);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+}
+
+function formHandler(event) {
+  event.preventDefault();
+
+  const { amount, delay, step } = inputKey;
+  for (let i = 0; i < amount; i++) {
+    if (i === 0) {
+      createPromise(1, Number(delay));
+    } else if (i === 1) {
+      createPromise(2, Number(delay) + Number(step));
+    } else {
+      createPromise(i + 1, Number(delay) + Number(step * i));
+    }
   }
 }
